7e82774a454d8f3893b3dd174c5cfd743d643e65
Linus Marco <lmarco163@gmail.com>
2018-05-13 12:55:19 -0400

    make sure nodes are sorted correctly

--- src/prep/parseData.js
+++ src/prep/parseData.js
@@ -10,7 +10,20 @@ rawData.elements.forEach((d, i) => {
     if (d.type === 'way') {
         if (i !== 0) {
             const lastWay = ways[ways.length - 1];
-            lastWay.nodes.forEach((n, j) => {
+
+            lastWay.nodeDetails.sort((a, b) => {
+                const idxA = lastWay.nodes.indexOf(a.id);
+                const idxB = lastWay.nodes.indexOf(b.id);
+
+                if (idxA === -1) console.error(`missing node : ${a.id}`);
+                if (idxB === -1) console.error(`missing node : ${b.id}`);
+
+                if (idxA > idxB) return 1;
+                else if (idxA < idxB) return -1;
+                else return 0;
+            });
+
+            lastWay.nodeDetails.forEach((n, j) => {
                 nodes.push({
                     wayId: lastWay.id,
                     nodeId: n.id,
@@ -21,10 +34,10 @@ rawData.elements.forEach((d, i) => {
             });
         }
 
-        d.nodes = [];
+        d.nodeDetails = [];
         ways.push(d);
     } else {
-        ways[ways.length - 1].nodes.push({
+        ways[ways.length - 1].nodeDetails.push({
             id: d.id,
             lat: d.lat,
             lon: d.lon
